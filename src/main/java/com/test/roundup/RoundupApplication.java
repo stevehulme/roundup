30f6842512c06e8146e18e89c8b40298ec6df9f0
Steve <smhulme@hotmail.com>
2018-08-11 16:52:31 +0100

    Initial commit

---
+++ src/main/java/com/test/roundup/RoundupApplication.java
@@ -0,0 +1,12 @@
+package com.test.roundup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoundupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoundupApplication.class, args);
+	}
+}
